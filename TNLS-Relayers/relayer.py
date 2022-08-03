9ffb76287dc71376f7db62eca3e76ce45055af80
FishmanL <leor@fractal.co>
2022-08-03 15:04:36 -0700

    more relayer/logging code

--- TNLS-Relayers/relayer.py
+++ TNLS-Relayers/relayer.py
@@ -23,34 +23,33 @@ from eth_interface import EthInterface, EthContract
 from scrt_interface import SCRTInterface, SCRTContract
 from base_interface import Task
 from threading import Thread
-from logging import getLogger
+from logging import getLogger, basicConfig, DEBUG, StreamHandler
 from time import sleep
 
 
 class Relayer:
-    def __init__(self, eth_interface: EthInterface, eth_contract_interface: EthContract, scrt_interface: SCRTInterface,
-                 scrt_contract_interface: SCRTContract):
+    def __init__(self, dict_of_names_to_interfaces):
         self.task_list = []
         self.task_threads = []
-        self.eth_interface = eth_interface
-        self.eth_contract_interface = eth_contract_interface
-        self.scrt_contract_interface = scrt_contract_interface
-        self.scrt_interface = scrt_interface
-        self.chain_interfaces = {'Ethereum': self.eth_interface, 'SCRT': self.scrt_interface}
-        self.contract_interfaces = {'Ethereum': self.eth_contract_interface, 'SCRT': self.scrt_contract_interface}
-        self.logger = getLogger(__name__)
+        self.dict_of_names_to_interfaces = dict_of_names_to_interfaces
+        basicConfig(
+            level=DEBUG,
+            format="%(asctime)s [%(levelname)8.8s] %(message)s",
+            handlers=[StreamHandler()],
+        )
+        self.logger = getLogger()
 
         pass
 
     def poll_for_transactions(self):
-        for name, interface in self.chain_interfaces.items():
-            transactions = interface.get_transactions()
+        for name, (chain_interface, contract_interface) in self.dict_of_names_to_interfaces.items():
+            transactions = chain_interface.get_transactions()
             for transaction in transactions:
-                task = self.contract_interfaces[name].parse_event_from_txn(transaction)
+                task = contract_interface.parse_event_from_txn(transaction)
                 self.task_list.append(task)
 
     def route_transaction(self, task: Task):
-        contract_for_txn = self.contract_interfaces[task.task_destination_network]
+        contract_for_txn = self.dict_of_names_to_interfaces[task.task_destination_network][1]
         function = contract_for_txn.get_function(task.task_data['function_name'])
         contract_for_txn.call_function(function, str(task))
         self.logger.info('Routed {} to {}'.format(task, task.task_destination_network))
@@ -61,17 +60,22 @@ class Relayer:
             while(len(self.task_list) > 0):
                 task = self.task_list.pop()
                 self.route_transaction(task)
-        if len(self.task_threads)<5:
+        if len(self.task_threads)<5 and len(self.task_list)>0:
             thread = Thread(target=thread_func)
             thread.start()
             self.task_threads.append(thread)
-            self.task_threads = [thread_live for thread_live in self.task_threads if thread_live.is_alive()]
+        self.task_threads = [thread_live for thread_live in self.task_threads if thread_live.is_alive()]
     def run(self):
         self.logger.info('Starting relayer')
         while True:
             self.poll_for_transactions()
-            self.logger.info('Polled for transactions, found {}'.format(len(self.task_list)))
+            self.logger.info('Polled for transactions, now have {} remaining'.format(len(self.task_list)))
             self.task_list_handle()
             self.logger.info('handled transactions')
             sleep(5)
         pass
+
+
+if __name__ == '__main__':
+    relayer = Relayer({})
+    relayer.run()
