7df6fc89a8197b37eb2e057c6c1b3d65ea48c5b0
FishmanL <leor@fractal.co>
2022-08-05 10:04:14 -0700

    fixing previous broken tests

--- TNLS-Relayers/relayer_tests/interface_tests/test_eth_interface.py
+++ TNLS-Relayers/relayer_tests/interface_tests/test_eth_interface.py
@@ -23,13 +23,38 @@ def non_send_provider(monkeypatch):
     yield web3provider
 
 
+
 @pytest.fixture
-def no_transaction_check_provider(non_send_provider, monkeypatch):
-    non_send_provider.transaction_retrieved = []
+def fake_provider(monkeypatch):
+
+
+    with open(f"{os.getcwd()}/infura_api_endpoint.txt") as file:
+        infura_endpoint = file.read()
+
+    API_MODE = "dev"
+    API_URL = infura_endpoint.replace("{ENDPOINT}", "mainnet") if API_MODE != "dev" else infura_endpoint.replace(
+        "{ENDPOINT}", "ropsten")
+
+    web3provider = Web3(Web3.HTTPProvider(API_URL))
+    web3provider.transaction_info = []
+
+
+    def mock_send_raw_transaction(tx):
+        web3provider.transaction_info.append(tx)
+        return tx
+
+
+    monkeypatch.setattr(web3provider.eth, 'send_raw_transaction', mock_send_raw_transaction)
+    monkeypatch.setattr(web3provider.eth, 'get_transaction_count', lambda _address: 1)
+    yield web3provider
+
+@pytest.fixture
+def no_transaction_check_provider(fake_provider, monkeypatch):
+    fake_provider.transaction_retrieved = []
 
     def mock_get_block(_block_number, **_kwargs):
         return {
-            'transactions': non_send_provider.transaction_retrieved
+            'transactions': fake_provider.transaction_retrieved
         }
 
     def mock_get_transaction_receipt(tx_hash):
@@ -49,9 +74,9 @@ def no_transaction_check_provider(non_send_provider, monkeypatch):
             ],
         }
 
-    monkeypatch.setattr(non_send_provider.eth, 'get_block', mock_get_block)
-    monkeypatch.setattr(non_send_provider.eth, 'get_transaction_receipt', mock_get_transaction_receipt)
-    return non_send_provider
+    monkeypatch.setattr(fake_provider.eth, 'get_block', mock_get_block)
+    monkeypatch.setattr(fake_provider.eth, 'get_transaction_receipt', mock_get_transaction_receipt)
+    return fake_provider
 
 
 @pytest.fixture
@@ -74,13 +99,13 @@ def sample_contract_function_factory():
     return _sample_contract_function
 
 
-def test_transaction_builder_good(non_send_provider, sample_contract_function_factory):
+def test_transaction_builder_good(fake_provider, sample_contract_function_factory):
     sample_private_key = '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f'
     sample_address = '0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377'
 
     # Note:  the below privkeys/addrs are published online
 
-    interface = EthInterface(address=sample_address, provider=non_send_provider,
+    interface = EthInterface(address=sample_address, provider=fake_provider,
                              private_key=sample_private_key)
     transaction = interface.create_transaction(sample_contract_function_factory(sample_address), '0x123')
     transaction.pop('gasPrice')
@@ -96,13 +121,13 @@ def test_transaction_builder_good(non_send_provider, sample_contract_function_fa
         transaction))) == '106996766325997339072655114405236224447014844012213495056075224830384823511471'
 
 
-def test_transaction_builder_bad_address_from(non_send_provider, sample_contract_function_factory):
+def test_transaction_builder_bad_address_from(fake_provider, sample_contract_function_factory):
     sample_private_key = '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f'
     sample_address = '0x0'
 
     # Note:  the below privkeys/addrs are published online
 
-    interface = EthInterface(address=sample_address, provider=non_send_provider,
+    interface = EthInterface(address=sample_address, provider=fake_provider,
                              private_key=sample_private_key)
     transaction = interface.create_transaction(sample_contract_function_factory(sample_address), '0x123')
     transaction.pop('gasPrice')
@@ -120,13 +145,13 @@ def test_transaction_builder_bad_address_from(non_send_provider, sample_contract
         excinfo.value)
 
 
-def test_transaction_builder_bad_address_to(non_send_provider, sample_contract_function_factory):
+def test_transaction_builder_bad_address_to(fake_provider, sample_contract_function_factory):
     sample_private_key = '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f'
     sample_address = '0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377'
 
     # Note:  the below privkeys/addrs are published online
 
-    interface = EthInterface(address=sample_address, provider=non_send_provider,
+    interface = EthInterface(address=sample_address, provider=fake_provider,
                              private_key=sample_private_key)
     transaction = interface.create_transaction(sample_contract_function_factory(sample_address), '0x123')
     transaction.pop('gasPrice')
@@ -146,13 +171,13 @@ def test_transaction_builder_bad_address_to(non_send_provider, sample_contract_f
     pass
 
 
-def test_transaction_builder_bad_private_key(non_send_provider, sample_contract_function_factory):
+def test_transaction_builder_bad_private_key(fake_provider, sample_contract_function_factory):
     sample_private_key = ''
     sample_address = '0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377'
 
     # Note:  the below privkeys/addrs are published online
 
-    interface = EthInterface(address=sample_address, provider=non_send_provider,
+    interface = EthInterface(address=sample_address, provider=fake_provider,
                              private_key=sample_private_key)
     transaction = interface.create_transaction(sample_contract_function_factory(sample_address), '0x123')
     transaction.pop('gasPrice')
@@ -171,13 +196,13 @@ def test_transaction_builder_bad_private_key(non_send_provider, sample_contract_
     pass
 
 
-def test_transaction_builder_mismatched_private_key(non_send_provider, sample_contract_function_factory):
+def test_transaction_builder_mismatched_private_key(fake_provider, sample_contract_function_factory):
     sample_private_key = '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8e'
     sample_address = '0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377'
 
     # Note:  the below privkeys/addrs are published online
 
-    interface = EthInterface(address=sample_address, provider=non_send_provider,
+    interface = EthInterface(address=sample_address, provider=fake_provider,
                              private_key=sample_private_key)
     transaction = interface.create_transaction(sample_contract_function_factory(sample_address), '0x123')
     transaction.pop('gasPrice')
