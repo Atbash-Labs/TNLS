ef2ebd13fd51a4b27a0ec52fbf3e0bf613b4fee9
FishmanL <leor@fractal.co>
2022-08-15 08:57:01 -0700

    first pass to config parsing

--- TNLS-Relayers/web_app.py
+++ TNLS-Relayers/web_app.py
@@ -1,16 +1,43 @@
 from threading import Thread
+from typing import Dict, Tuple
 
 from flask import Flask, current_app, Blueprint
+from yaml import safe_load
 
+from base_interface import BaseChainInterface, BaseContractInterface
+from eth_interface import EthInterface, EthContract
 from relayer import Relayer
 
+base_map = {'Ethereum': (EthInterface, EthContract)}
 
-def convert_config_file_to_dict(_config_file) -> dict:
-    config_dict = {}
-    with open(_config_file) as f:
-        for line in f.readlines():
-            key, val = line.split('=')
-            config_dict[key] = val
+
+def convert_config_file_to_dict(config_file, map_of_names_to_interfaces=None) -> \
+        Dict[str, Tuple[BaseChainInterface, BaseContractInterface, str, str]]:
+    """
+    Converts a yml config file into a dict of names to interfaces for the relayer
+    Args:
+        config_file: the config file to pull from
+        map_of_names_to_interfaces: an initial map of names to the classes they should be instantiated as
+
+    Returns: a dict mapping names to interfaces as the relayer expects
+
+    """
+    if map_of_names_to_interfaces is None:
+        map_of_names_to_interfaces = base_map
+    with open(config_file) as f:
+        config_dict = safe_load(f)
+    necessary_keys = ['contract_address', 'contract_schema', 'private_key', 'wallet_address',
+                      'event_name', 'function_name']
+    for key, val in config_dict.items():
+        if not all(val_key in val for val_key in necessary_keys):
+            raise ValueError(f'{key} is missing necessary keys: {set(necessary_keys) - set(val.keys())}')
+        if key not in map_of_names_to_interfaces:
+            raise ValueError(f'{key} not in map of names to interfaces')
+        chain_interface, contract_interface = map_of_names_to_interfaces[key]
+        initialized_chain = chain_interface(private_key=val['private_key'], address=val['wallet_address'])
+        initialized_contract = contract_interface(interface=initialized_chain, address=val['contract_address'],
+                                                  abi=val['contract_schema'])
+        config_dict[key] = (initialized_chain, initialized_contract, val['event_name'], val['function_name'])
     return config_dict
 
 
