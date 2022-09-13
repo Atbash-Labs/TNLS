5216f25f9a666429b172a00365363f87f5a96d07
Prince Fefar <pfefar1@gmail.com>
2022-09-12 17:07:08 -0700

    add callback mock

--- public-gateway/src/Client.sol
+++ public-gateway/src/Client.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+contract Client {
+    
+    /// @param _taskId  Task Id of the computation
+    /// @param _result computed result
+    /// @param _result The second stored number input
+    function callback(uint256 _taskId, bytes memory _result, bytes memory _resultSig) external {
 
-contract Client{
 
 
 
 
-  
+      
+    }
 }
