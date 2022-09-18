fe6835952bd4b330e204bccaa7959b37e935e6ba
Prince Fefar <pfefar1@gmail.com>
2022-09-17 23:56:07 -0700

    modified script

--- public-gateway/script/DeployScript.s.sol
+++ public-gateway/script/DeployScript.s.sol
@@ -22,5 +22,38 @@ contract DeployScript is Script {
         console2.logAddress(address(clientAddress));
 
         vm.stopBroadcast();
+
+        /// ----    DO IT THISSS WAY -------  //////
+        
+        // GatewayDub gatewayAddress;
+        // ClientDub clientAddress;
+        // uint256 privKey = vm.envUint("PRIVATE_KEY");
+        // address deployer = vm.rememberKey(privKey);
+        // uint256 verificationAddress = vm.envAddress("SECRET_DERIVED_ETHADDRESS");
+        // string memory route = "secret";
+
+        // // Update the route with with masterVerificationKey signature
+        // bytes32 routeHash = getRouteHash(route, verificationAddress);
+        // bytes32 ethSignedMessageHash = getEthSignedMessageHash(routeHash);
+
+        // (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, ethSignedMessageHash);
+        // bytes memory sig = abi.encodePacked(r, s, v);
+
+        // vm.startBroadcast();
+        
+        // gatewayAddress = new GatewayDub();
+        // clientAddress = new ClientDub(address(gatewayAddress));
+        
+        // console2.logAddress(address(gatewayAddress));
+        // console2.logAddress(address(clientAddress));
+        // console2.logAddress(deployer);
+
+        // gatewayAddress.initialize(deployer);
+
+        // gatewayAddress.updateRoute(route, verificationAddress, sig);
+
+        // vm.stopBroadcast();
+
+
     }
 }
