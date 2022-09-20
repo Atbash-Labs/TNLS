841e012589a5ae41561eb85ef8e0a0b0ddee0b94
kent-3 <100624004+kent-3@users.noreply.github.com>
2022-09-20 12:35:13 -0400

    simplify to one button, display useful info

--- TNLS-Clients/payload-encryption/src/submit.ts
+++ TNLS-Clients/payload-encryption/src/submit.ts
@@ -1,9 +1,24 @@
-import { ethers } from 'ethers'
+import { encrypt_payload } from "./wasm";
+import { ethers } from "ethers";
+import { arrayify, SigningKey } from "ethers/lib/utils";
+import { Buffer } from "buffer/";
+import secureRandom from "secure-random";
 
 export async function setupSubmit(element: HTMLButtonElement) {
     // @ts-ignore
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const [myAddress] = await provider.send("eth_requestAccounts", []);
+
+    // generating ephemeral keys
+    const wallet = ethers.Wallet.createRandom();
+    const userPrivateKeyBytes = arrayify(wallet.privateKey);
+    const userPublicKey: string = new SigningKey(wallet.privateKey).compressedPublicKey;
+    const userPublicKeyBytes = arrayify(userPublicKey)
+    //
+
+    const gatewayPublicKey = "AnPh4zgH57ijrnAEaTxQMtBJSYIe9fJruWRRD8JLt+Cf"; // TODO get this key
+    const gatewayPublicKeyBuffer = Buffer.from(gatewayPublicKey, "base64");
+    const gatewayPublicKeyBytes = arrayify(gatewayPublicKeyBuffer);
 
     element.addEventListener("click", async function(event: Event){
         event.preventDefault()
@@ -23,24 +38,47 @@ export async function setupSubmit(element: HTMLButtonElement) {
         income: Number(income)
         })
         console.log(data)
+
+        const routing_info = "secret10pyejy66429refv3g35g2t7am0was7ya6hvrzf"
+        const routing_code_hash = "5daf336102c875f790dfeabb23a4d985bf71a9856b6cb52be00fd7e9de41ce32"
+        const user_address = myAddress
+        const user_key = Buffer.from(userPublicKeyBytes).toString('base64');
+    
+        const thePayload = JSON.stringify({
+          data: data,
+          routing_info: routing_info,
+          routing_code_hash: routing_code_hash,
+          user_address: user_address,
+          user_key: user_key,
+        })
+        console.log(thePayload)
         
+        const plaintext = Buffer.from(JSON.stringify(thePayload));
+        const nonce = secureRandom(12, { type: "Uint8Array" });
+
+        const ciphertext = Buffer.from(
+        encrypt_payload(
+            gatewayPublicKeyBytes,
+            userPrivateKeyBytes,
+            plaintext,
+            nonce
+        ));
+        console.log(`Encrypted Payload: ${ciphertext.toString('base64')}`)
+    
+        const hashedMsg = ethers.utils.keccak256(arrayify(ciphertext))
+        console.log(`Message hash: ${hashedMsg}`)
+
         document.querySelector<HTMLDivElement>('#preview')!.innerHTML = `
-        <p >
-        ${data}
+        <h2>Raw Payload</h2>
+        <p>${thePayload}</p>
+        <h2>Encrypted Payload</h2>
+        <p>${ciphertext.toString('base64')}</p>
+        <h2>Other Info</h2>
+        <p>
+        Encryption method: ChaCha20Poly1305 <br>
+        Nonce used during encryption: ${nonce} <br>
+        Message Hash: ${hashedMsg} <br>
         </p>
         `
-
-        // document.querySelector<HTMLDivElement>('#test')!.innerHTML = `
-        //   <p>
-        //   "data": {<br>
-        //     &emsp;"address":"${myAddress}",<br>
-        //     &emsp;"offchain_assets":${input1},<br>
-        //     &emsp;"onchain_assets":${input2},<br>
-        //     &emsp;"liabilities":${input3},<br>
-        //     &emsp;"missed_payments":${input4},<br>
-        //     &emsp;"income":${input5}<br>
-        //   }
-        //   </p>
-        // `
     })
 }
